ff7d5bef0b631095c7a9362735b715e056876b4e
Eduardo de Brito Soares <du.ebrits@gmail.com>
2025-04-07 18:31:26 -0300

    first commit

---
+++ support-tickets/src/server.js
@@ -0,0 +1,11 @@
+import http from "node:http";
+
+import { jsonHandler } from "./middlewares/jsonHandler.js";
+import { routeHandler } from "./middlewares/routeHandler.js";
+
+async function listerner(request, response) {
+  await jsonHandler(request, response);
+  routeHandler(request, response);
+}
+
+http.createServer(listerner).listen(3333);
